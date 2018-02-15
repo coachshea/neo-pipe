5383ebd19b615968840e9fb2a465c786a4f09c55
coachshea <coachshea@fastmail.com>
2018-02-15 14:35:33 -0500

    wip

---
+++ t/main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define CTRLD   4
+
+/**
+ * to store an int (id) into void*
+ *
+ * void *ptr = malloc(sizeof(int));
+ * *((int*)ptr) = 5;
+ * printf("%d\n",*((int*)ptr));
+ */
+
+int
+main()
+{
+  printf("hello %s", "john");
+}
---
+++ t/trial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define CTRLD   4
+
+/**
+ * to store an int (id) into void*
+ *
+ * void *ptr = malloc(sizeof(int));
+ * *((int*)ptr) = 5;
+ * printf("%d\n",*((int*)ptr));
+ */
+
+int
+main()
+{
+  printf("hello %s", "john");
+}
